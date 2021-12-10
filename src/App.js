e3b229a1279b7f0bf71f6eec7d21b738ed7660ce
Abdul Kader <abdulkadirshohan@gmail.com>
2021-12-10 21:30:14 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-
+import sample from './video.mp4';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+            <section className="showcase">
+
+                <video src={sample} muted loop autoPlay />
+                <div className="overlay" />
+                <div className="text">
+                    <h2>Never Stop To </h2>
+                    <h3>Exploring The World</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat.</p>
+                    <a href="#">Explore</a>
+                </div>
+                <ul className="social">
+                    <li><a href="#"><img src="https://i.ibb.co/x7P24fL/facebook.png" /></a></li>
+                    <li><a href="#"><img src="https://i.ibb.co/Wnxq2Nq/twitter.png" /></a></li>
+                    <li><a href="#"><img src="https://i.ibb.co/ySwtH4B/instagram.png" /></a></li>
+                </ul>
+            </section>
+            <div className="menu">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">News</a></li>
+                    <li><a href="#">Destination</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
     </div>
   );
 }
